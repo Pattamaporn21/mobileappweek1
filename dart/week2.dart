aa60521930eb32596b739516cd59bb50adae2257
Pattamaporn <s6302041520082@email.kmutnbac.th>
2021-08-05 17:12:29 +0700

    เพิ่มcomment1

--- dart/week2.dart
+++ dart/week2.dart
@@ -3,11 +3,12 @@ void main() {
 
  // var x = callColor("black");
  // print(x);
+ //เรียกใช้ function , สามารถใส่ตัวไหนก็ได้ไม่จำเป็นต้องเรียง
  callwed("google" , protocal: "https", port: 443);
 }
 
 String callGen(int year) => (year >= 1996) ? "Gen Z" : "Gen Y";
-/******Test1*********/
+//
 
 int callColor([String newColor = "pink"]) {
 
@@ -15,10 +16,12 @@ int callColor([String newColor = "pink"]) {
 
   color.add(newColor);
 
-  for (var item in color) {
+//คำสั่ง forin ทำซ้ำร่วมกับ list array
+  for (var item in color) { 
     print(item);
   }
   return color.length;
 }
+//เรียกใช้ function Arrow , การใช้ Named parameter,และ setค่าเริ่มต้น
 void callwed(String web ,{int port =80 , String protocal ="http"}) => print(" $protocal $web : $port");
 
